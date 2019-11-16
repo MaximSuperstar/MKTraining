9637a3010f366d46451fb85750972cf49d945fdc
Maxim-1975 <mkolpakov@gmail.com>
2019-11-16 23:26:51 +0300

    ContactRemovalTests test was added

--- Second-Solution-Attempt-2/Second-Solution-Attempt-2/appmanager/ContactHelper.cs
+++ Second-Solution-Attempt-2/Second-Solution-Attempt-2/appmanager/ContactHelper.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
 
 namespace WebAddressbookTests
 {
     public class ContactHelper:HelperBase
-    {    
+    {
+        protected bool acceptNextAlert = true;
         public ContactHelper(ApplicationManager manager)
             :base(manager)
         {}
@@ -29,14 +33,9 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("lastname")).SendKeys(icd.Lastename);            
         }
 
-        public ContactHelper Contacts_Modify(string index, InitContactData icd_modified)
+        public ContactHelper SelectContact(string index)
         {
-            manager.Navigators.OpenHomePage();
-            driver.FindElement(By.Id(index)).Click();
-            driver.FindElement(By.XPath("(//img[@alt='Edit'])["+ index+"]")).Click();
-            InitContactCreation(icd_modified);
-            SubmitContactCreation();
-            manager.Navigators.ReturnHomePage();
+            driver.FindElement(By.XPath("//table[@id='maintable']/tbody/tr[" + index + "]/td/input")).Click();
             return this;
         }
 
@@ -51,5 +50,48 @@ namespace WebAddressbookTests
             manager.Navigators.ReturnHomePage();
             return this;
         }
+        public ContactHelper Contacts_Modify(string index, InitContactData icd_modified)
+        {
+            manager.Navigators.OpenHomePage();
+            SelectContact(index);
+            driver.FindElement(By.XPath("(//img[@alt='Edit'])[" + index + "]")).Click();
+            InitContactCreation(icd_modified);
+            SubmitContactCreation();
+            manager.Navigators.ReturnHomePage();
+            return this;
+        }
+        public ContactHelper RemoveContact(string index)
+        {
+            manager.Navigators.OpenHomePage();
+            SelectContact(index);
+            acceptNextAlert = true;
+            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
+            Assert.IsTrue(Regex.IsMatch(CloseAlertAndGetItsText(), "^Delete 1 addresses[\\s\\S]$"));
+            manager.Navigators.ReturnHomePage();
+            return this;
+        }
+        //delete--cancel confirmation methods//////////////////////////
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+        /////////////////////////////////////////////////////////////////
     }
 }
